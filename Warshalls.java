2c551869bdcce872de0c0f1da8701adf6d2fb9eb
Ajith <75195113+Ajith-S1@users.noreply.github.com>
2022-08-11 19:45:24 +0530

    Create Warshalls.java

---
+++ Warshalls.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Warshals {
+
+	static void warshals(short a[][],int n)
+	{
+		for(int k=0;k<n;k++)
+			for(int i=0;i<n;i++)
+				for(int j=0;j<n;j++)
+						a[i][j] = (short) (a[i][j] | ( a[i][k]  & a[k][j] )) ;
+				
+	}
+	
+	static void display(short a[][],int n)
+	{
+		System.out.println("new cost matrix : ");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+				System.out.print(a[i][j] + " ");
+			System.out.println();
+		}
+			
+	}
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter no of vertex");
+		int n=sc.nextInt();
+		short costm[][]= new short[n][n];
+		
+		System.out.println("Enter cost matrix");
+		for(int i=0;i<n;i++)
+				for(int j=0;j<n;j++)
+					costm[i][j]= sc.nextShort();
+		warshals(costm,n);
+		display(costm,n);
+		sc.close();
+	}
+
+}
+
